18c8f9daaae2b49a7f97b31034a76fe3b64fe0e9
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-08-28 19:28:35 -0500

    return my key binds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -59,4 +59,66 @@ vim.lsp.config.ruff = {
 -- Enable the LSP servers
 vim.lsp.enable({ "pylsp", "ruff" })
 
+-- Setup LSP keymaps
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set(
+			"n",
+			"gD",
+			vim.lsp.buf.declaration,
+			vim.tbl_extend("force", opts, { desc = "Go to declaration" })
+		)
+		vim.keymap.set(
+			"n",
+			"gd",
+			vim.lsp.buf.definition,
+			vim.tbl_extend("force", opts, { desc = "Go to definition" })
+		)
+		vim.keymap.set(
+			"n",
+			"K",
+			vim.lsp.buf.hover,
+			vim.tbl_extend("force", opts, { desc = "Hover documentation" })
+		)
+		vim.keymap.set(
+			"n",
+			"gi",
+			vim.lsp.buf.implementation,
+			vim.tbl_extend("force", opts, { desc = "Go to implementation" })
+		)
+		vim.keymap.set(
+			"n",
+			"<C-k>",
+			vim.lsp.buf.signature_help,
+			vim.tbl_extend("force", opts, { desc = "Signature help" })
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>rn",
+			vim.lsp.buf.rename,
+			vim.tbl_extend("force", opts, { desc = "Rename symbol" })
+		)
+		vim.keymap.set(
+			{ "n", "v" },
+			"<leader>ca",
+			vim.lsp.buf.code_action,
+			vim.tbl_extend("force", opts, { desc = "Code action" })
+		)
+		vim.keymap.set(
+			"n",
+			"gr",
+			vim.lsp.buf.references,
+			vim.tbl_extend("force", opts, { desc = "Go to references" })
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>e",
+			vim.diagnostic.open_float,
+			vim.tbl_extend("force", opts, { desc = "Show line diagnostics" })
+		)
+	end,
+})
+
 return {}
