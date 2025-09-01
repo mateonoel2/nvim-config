8a1d52d05759271aac7a6e79c82fa306a5ecd937
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-09-01 17:41:31 -0500

    return of mason

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,21 +1,44 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      "<leader>F",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
-    },
-  },
-  opts = {
-    formatters_by_ft = {
-      python = { "ruff_format", "ruff_organize_imports" },
-      lua = { "stylua" },
-    },
-  },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>F",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			python = { "ruff_format", "ruff_organize_imports" },
+			lua = { "stylua" },
+			javascript = function(bufnr)
+				local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "biome" })
+				return #clients > 0 and { "biome" } or { "prettier" }
+			end,
+			javascriptreact = function(bufnr)
+				local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "biome" })
+				return #clients > 0 and { "biome" } or { "prettier" }
+			end,
+			typescript = function(bufnr)
+				local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "biome" })
+				return #clients > 0 and { "biome" } or { "prettier" }
+			end,
+			typescriptreact = function(bufnr)
+				local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "biome" })
+				return #clients > 0 and { "biome" } or { "prettier" }
+			end,
+			json = function(bufnr)
+				local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "biome" })
+				return #clients > 0 and { "biome" } or { "prettier" }
+			end,
+			css = { "prettier" },
+			html = { "prettier" },
+			markdown = { "prettier" },
+		},
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,155 +1,83 @@
--- Configure pylsp with native LSP config
-vim.lsp.config.pylsp = {
-	cmd = { "pylsp" },
-	filetypes = { "python" },
-	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
-	offset_encoding = "utf-8",
-	on_init = function(client, config)
-		-- Check for mypy configuration
-		local has_mypy_config = false
-		local root_dir = client.config.root_dir or vim.fn.getcwd()
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pylsp", "ruff", "biome", "eslint" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      
+      -- Setup LSP servers
+      lspconfig.pylsp.setup({})
+      lspconfig.ruff.setup({
+        on_attach = function(client, bufnr)
+          client.server_capabilities.hoverProvider = false
+        end,
+      })
+      lspconfig.biome.setup({})
+      lspconfig.eslint.setup({})
 
-		-- Check for mypy.ini or .mypy.ini
-		if vim.fn.filereadable(root_dir .. "/mypy.ini") == 1 or vim.fn.filereadable(root_dir .. "/.mypy.ini") == 1 then
-			has_mypy_config = true
-		end
+      -- Enable automatic completion on text change
+      vim.api.nvim_create_autocmd("TextChangedI", {
+        callback = function()
+          local line = vim.api.nvim_get_current_line()
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          if col > 0 and line:sub(col, col):match("[%w%.]") then
+            vim.lsp.buf.completion()
+          end
+        end,
+      })
 
-		-- Check for mypy in pyproject.toml
-		if not has_mypy_config then
-			local pyproject_path = root_dir .. "/pyproject.toml"
-			if vim.fn.filereadable(pyproject_path) == 1 then
-				local content = table.concat(vim.fn.readfile(pyproject_path), "\n")
-				if content:match("%[tool%.mypy") then
-					has_mypy_config = true
-				end
-			end
-		end
+      -- Tab completion mapping
+      vim.keymap.set("i", "<Tab>", function()
+        if vim.fn.pumvisible() == 1 then
+          return "<C-n>"
+        else
+          return "<Tab>"
+        end
+      end, { expr = true })
 
-		-- Update settings with mypy plugin if config found
-		client.config.settings = client.config.settings or {}
-		client.config.settings.pylsp = client.config.settings.pylsp or {}
-		client.config.settings.pylsp.plugins = client.config.settings.pylsp.plugins or {}
-		client.config.settings.pylsp.plugins.pylsp_mypy = { enabled = has_mypy_config }
-	end,
-	settings = {
-		pylsp = {
-			plugins = {
-				pycodestyle = { enabled = false },
-				pylsp_mypy = { enabled = false }, -- Will be dynamically set
-			},
-		},
-	},
+      vim.keymap.set("i", "<S-Tab>", function()
+        if vim.fn.pumvisible() == 1 then
+          return "<C-p>"
+        else
+          return "<S-Tab>"
+        end
+      end, { expr = true })
+
+      -- Setup LSP keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+
+          -- Enable omnifunc completion
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature help" }))
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Go to references" }))
+          vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show line diagnostics" }))
+        end,
+      })
+    end,
+  },
 }
-
--- Configure ruff with native LSP config
-vim.lsp.config.ruff = {
-	cmd = { "ruff", "server" },
-	filetypes = { "python" },
-	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
-	offset_encoding = "utf-8",
-	on_attach = function(client, bufnr)
-		client.server_capabilities.hoverProvider = false
-	end,
-	settings = {
-		logLevel = "error",
-		logFile = "/dev/null",
-	},
-}
-
--- Enable the LSP servers
-vim.lsp.enable({ "pylsp", "ruff" })
-
--- Enable automatic completion on text change
-vim.api.nvim_create_autocmd("TextChangedI", {
-	callback = function()
-		local line = vim.api.nvim_get_current_line()
-		local col = vim.api.nvim_win_get_cursor(0)[2]
-		if col > 0 and line:sub(col, col):match("[%w%.]") then
-			vim.lsp.buf.completion()
-		end
-	end,
-})
-
--- Tab completion mapping
-vim.keymap.set("i", "<Tab>", function()
-	if vim.fn.pumvisible() == 1 then
-		return "<C-n>"
-	else
-		return "<Tab>"
-	end
-end, { expr = true })
-
-vim.keymap.set("i", "<S-Tab>", function()
-	if vim.fn.pumvisible() == 1 then
-		return "<C-p>"
-	else
-		return "<S-Tab>"
-	end
-end, { expr = true })
-
--- Setup LSP keymaps
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		local opts = { buffer = ev.buf }
-		
-		-- Enable omnifunc completion
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-		vim.keymap.set(
-			"n",
-			"gD",
-			vim.lsp.buf.declaration,
-			vim.tbl_extend("force", opts, { desc = "Go to declaration" })
-		)
-		vim.keymap.set(
-			"n",
-			"gd",
-			vim.lsp.buf.definition,
-			vim.tbl_extend("force", opts, { desc = "Go to definition" })
-		)
-		vim.keymap.set(
-			"n",
-			"K",
-			vim.lsp.buf.hover,
-			vim.tbl_extend("force", opts, { desc = "Hover documentation" })
-		)
-		vim.keymap.set(
-			"n",
-			"gi",
-			vim.lsp.buf.implementation,
-			vim.tbl_extend("force", opts, { desc = "Go to implementation" })
-		)
-		vim.keymap.set(
-			"n",
-			"<C-k>",
-			vim.lsp.buf.signature_help,
-			vim.tbl_extend("force", opts, { desc = "Signature help" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>rn",
-			vim.lsp.buf.rename,
-			vim.tbl_extend("force", opts, { desc = "Rename symbol" })
-		)
-		vim.keymap.set(
-			{ "n", "v" },
-			"<leader>ca",
-			vim.lsp.buf.code_action,
-			vim.tbl_extend("force", opts, { desc = "Code action" })
-		)
-		vim.keymap.set(
-			"n",
-			"gr",
-			vim.lsp.buf.references,
-			vim.tbl_extend("force", opts, { desc = "Go to references" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>e",
-			vim.diagnostic.open_float,
-			vim.tbl_extend("force", opts, { desc = "Show line diagnostics" })
-		)
-	end,
-})
-
-return {}
